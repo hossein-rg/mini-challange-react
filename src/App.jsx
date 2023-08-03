f8c8e95b00017b9b5a08da6b4b6c9d50029c1d35
hosinrg1380@gmail.com <hosinrg1380@gmail.com>
2023-08-03 23:04:35 +0330

    tip-calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.scss";
 import CardsLaptop from "./components/card-laptop/CardsLaptop";
+import TipCalc from "./components/tip-calculator/TipCalc";
 function App() {
   return (
     <>
       <div>
         <p className="helloworld">hello world</p>
         <CardsLaptop />
+        <TipCalc />
       </div>
     </>
   );
