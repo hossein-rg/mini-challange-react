98d7def359113981361af1b6e266738360ffacda
hosinrg1380@gmail.com <hosinrg1380@gmail.com>
2023-08-04 18:59:47 +0330

    convert people function to class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.scss";
-import CardsLaptop from "./components/card-laptop/CardsLaptop";
+// import CardsLaptop from "./components/card-laptop/CardsLaptop";
 import TipCalc from "./components/tip-calculator/TipCalc";
 function App() {
   return (
     <>
       <div>
-        <p className="helloworld">hello world</p>
-        <CardsLaptop />
+        {/* <p className="helloworld">hello world</p>
+        <CardsLaptop /> */}
         <TipCalc />
       </div>
     </>
