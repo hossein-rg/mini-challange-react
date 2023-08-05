c9a7665a4dd99658488e6202a125bfe273bb4756
hosinrg1380@gmail.com <hosinrg1380@gmail.com>
2023-08-05 03:34:00 +0330

    set jsx in challange 3 without sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.scss";
+import PassGen from "./components/password-generator/PassGen";
 // import CardsLaptop from "./components/card-laptop/CardsLaptop";
-import TipCalc from "./components/tip-calculator/TipCalc";
+// import TipCalc from "./components/tip-calculator/TipCalc";
 function App() {
   return (
     <>
       <div>
         {/* <p className="helloworld">hello world</p>
         <CardsLaptop /> */}
-        <TipCalc />
+        {/* <TipCalc /> */}
+        <PassGen />
       </div>
     </>
   );
--- src/components/password-generator/PassGen.jsx
+++ src/components/password-generator/PassGen.jsx
@@ -1,6 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React, { Component } from 'react';
-import styles from "../../assets/styles/pass-gen/passGen.module.scss"
+import styles from "../../assets/styles/pass-gen/passGen.module.scss";
+
+import iconCopy from "../../assets/images/pass-gen/iconcopy.png";
 class PassGen extends Component{
     constructor(props){
         super(props);
@@ -14,8 +16,47 @@ class PassGen extends Component{
     }
     render(){
         return(
-            <div className={styles.div}>
-                <p>ok</p>
+            <div className={styles.container}>
+                <h2>Password Generator</h2>
+                <div className={styles.container_passWasCreate}>
+                    <p>Pd43pj9r!</p>
+                    <img src={iconCopy} alt="iconCopy" />
+                </div>
+                <div className={styles.container_createPass}>
+                    <div className={styles.passLength}>
+                        <div>
+                            <p>Character Length</p>
+                            <p>0</p>
+                        </div>
+                        <div>
+                            <input type="range" name="rangeLen"/>
+                        </div>
+                    </div>
+                    <div className={styles.passStrength}>
+                        <ul>
+                            <li>
+                            <input type="checkbox" name="uppercase" />
+                            <p>Include Uppercase Letters</p>
+                            </li>
+                            <li>
+                            <input type="checkbox" name="lowercase" />
+                            <p>Include Lowercase Letters</p>
+                            </li>
+                            <li>
+                            <input type="checkbox" name="number" />
+                            <p>Include Numbers</p>
+                            </li>
+                            <li>
+                            <input type="checkbox" name="symbols" />
+                            <p>Include Symbols</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.showStrength}>
+                        <p>STRENGTH</p>
+                        <p>🟢🟢🟢🟢🟢🟢</p>
+                    </div>
+                </div>
             </div>
         )
     }
