408968c6b1b85d8740a5ccd849177e8f953e674c
hosinrg1380@gmail.com <hosinrg1380@gmail.com>
2023-08-10 22:31:58 +0330

    fix check inputs(name,email,phone) and pass validate to component multiStepForm

--- src/components/multi-step-form/MultiStepForm.jsx
+++ src/components/multi-step-form/MultiStepForm.jsx
@@ -12,6 +12,30 @@ class MultiStepForm extends React.Component{
         this.btnNext = React.createRef();
         this.state = {
             whereLocate : 0,
+            checkedFullname : false,
+            checkedEmail : false,
+            checkedPhone : false
+        }
+    }
+
+    checkPersonalInfo = (how,data)=>{
+        if(how == "name"){
+            console.log(data)
+            this.setState({
+                checkedFullname : data,
+            })
+        }
+        else if(how == "email"){
+            console.log(data)
+            this.setState({
+                checkedEmail: data,
+                });
+        }
+        else if(how == "phone"){
+            console.log(data)
+            this.setState({
+                checkedPhone :data,
+            })
         }
     }
 
@@ -26,10 +50,14 @@ class MultiStepForm extends React.Component{
             whereLocate : this.state.whereLocate - 1,
         })
         }else{
-            backBtn.style.display = "block";
-            this.setState({
-                whereLocate : this.state.whereLocate + 1,
-            })
+            if(this.state.whereLocate == 0){
+                if(this.state.checkedFullname && this.state.checkedEmail && this.state.checkedPhone){
+                    backBtn.style.display = "block";
+                    this.setState({
+                        whereLocate : this.state.whereLocate + 1,
+                    })
+                }
+            }
         }
     }
 
@@ -37,12 +65,12 @@ class MultiStepForm extends React.Component{
         const {whereLocate} = this.state;
         return(
             <div>
-            {whereLocate == 0 ? <PersonalInfo /> : <></>}
+            {whereLocate == 0 ? <PersonalInfo check = {this.checkPersonalInfo}/> : <></>}
             {whereLocate == 1 ? <SelectPlan /> : <></>}
             {whereLocate == 2 ? <AddOns /> : <></>}
             {whereLocate == 3 ? <FinishingUp /> : <></>}
             {whereLocate == 4 ? <FinishingUp /> : <></>}
-            <button ref={this.btnBack} data-click="0" onClick={this.clickWherePage}>Go Back</button>
+            <button style={{display:"none"}} ref={this.btnBack} data-click="0" onClick={this.clickWherePage}>Go Back</button>
             <button ref={this.btnNext} data-click="1" onClick={this.clickWherePage}>Next Step {whereLocate}</button>
             </div>
         )
--- src/components/multi-step-form/PersonalInfo.jsx
+++ src/components/multi-step-form/PersonalInfo.jsx
@@ -3,6 +3,32 @@ import styles from '../../assets/styles/multi-step-form/multiStep.module.scss';
 class PersonalInfo extends React.Component{
     constructor(props){
         super(props);
+        this.inputName = React.createRef();
+        this.state={
+            nameValue:"",
+        }
+    }
+
+
+    clickCheckInputs = (e)=>{
+        let dataset = e.target.dataset.set;
+        const value = e.target.value.toLowerCase();
+        if(dataset == "name"){
+            const checkValue = /^[a-z]*$/g.test(value)
+            // eslint-disable-next-line react/prop-types
+            return this.props.check("name",checkValue);
+        }
+        else if(dataset == "email"){
+            // eslint-disable-next-line no-useless-escape
+            const checkValue = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)
+            // eslint-disable-next-line react/prop-types
+            return this.props.check("email",checkValue);
+        }
+        else if(dataset == "phone"){
+            const checkValue = /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/.test(value);
+            // eslint-disable-next-line react/prop-types
+            return this.props.check("phone",checkValue)
+        }
     }
 
     render(){
@@ -13,16 +39,16 @@ class PersonalInfo extends React.Component{
                 <div className={styles.form_PersonalInfo}>
                     <form>
                         <div>
-                            <label>Name</label>
-                            <input type="text" name="name" required placeholder="e.g. Hossein Rg"/>
+                            <label>Your name</label>
+                            <input onChange={this.clickCheckInputs} ref={this.inputName} type="text" name="name" required placeholder="e.g. Hossein" data-set="name"/>
                         </div>
                         <div>
                             <label>Email Address</label>
-                            <input type="email" name="email" required placeholder="e.g. hosseinrg@gmail.com"/>
+                            <input onChange={this.clickCheckInputs} data-set="email" type="email" name="email" required placeholder="e.g. hosseinrg@gmail.com"/>
                         </div>
                         <div>
                             <label>Phone Number</label>
-                            <input type="tel" name="tel" required placeholder="e.g. +98 990 999 90 90"/>
+                            <input onChange={this.clickCheckInputs} data-set="phone" type="tel" name="tel" required placeholder="e.g. 09123331210"/>
                         </div>
                         {/* <input type="submit" value="Next Step" /> */}
                     </form>
