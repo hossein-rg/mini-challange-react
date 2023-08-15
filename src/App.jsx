92187ec2d0116484ec2f15e1627d1a0a916ccb73
hosinrg1380@gmail.com <hosinrg1380@gmail.com>
2023-08-15 20:58:05 +0330

    setup initial router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import './App.scss'
-import MultiStepForm from './components/multi-step-form/MultiStepForm'
-// import PassGen from "./components/password-generator/PassGen";
-// import CardsLaptop from "./components/card-laptop/CardsLaptop";
-// import TipCalc from "./components/tip-calculator/TipCalc";
+import "./App.scss";
+import MultiStepForm from "./components/multi-step-form/MultiStepForm";
+import PassGen from "./components/password-generator/PassGen";
+import CardsLaptop from "./components/card-laptop/CardsLaptop";
+import TipCalc from "./components/tip-calculator/TipCalc";
+import { Route, Switch, Link } from "react-router-dom";
 function App() {
-    return (
-        <>
-            <div>
-                {/* <p className="helloworld">hello world</p>
-        <CardsLaptop /> */}
-                {/* <TipCalc /> */}
-                {/* <PassGen /> */}
-                <MultiStepForm />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <p className="helloworld">hello world</p>
+        <CardsLaptop />
+        <TipCalc />
+        <PassGen />
+        <MultiStepForm />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
