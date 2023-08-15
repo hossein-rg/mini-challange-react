8ebc5c05efd301afeb63121cae1f5eb0f6f5dc10
hosinrg1380@gmail.com <hosinrg1380@gmail.com>
2023-08-15 22:30:07 +0330

    setup finally route

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,32 @@ import MultiStepForm from "./components/multi-step-form/MultiStepForm";
 import PassGen from "./components/password-generator/PassGen";
 import CardsLaptop from "./components/card-laptop/CardsLaptop";
 import TipCalc from "./components/tip-calculator/TipCalc";
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 function App() {
   return (
-    <>
+    <div>
+      <p className="helloworld">hello world</p>
+      <ul>
+        <li>
+          <Link to="/cardslaptop">Cards laptop</Link>
+        </li>
+        <li>
+          <Link to="/tipcalc">Tip Calculator</Link>
+        </li>
+        <li>
+          <Link to="/passgen">Password Generator</Link>
+        </li>
+        <li>
+          <Link to="/multistep">multi step forms</Link>
+        </li>
+      </ul>
       <div>
-        <p className="helloworld">hello world</p>
-        <CardsLaptop />
-        <TipCalc />
-        <PassGen />
-        <MultiStepForm />
+        <Route path="/cardslaptop" component={CardsLaptop} />
+        <Route path="/tipcalc" component={TipCalc} />
+        <Route path="/passgen" component={PassGen} />
+        <Route path="/multistep" component={MultiStepForm} />
       </div>
-    </>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
